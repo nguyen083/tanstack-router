84574b2059946163c3ab17922c229c7f6ec3824e
NguyênNguyên <dev.nguyennguyen@gmail.com>
2025-08-30 10:05:48 +0700

    feat: Add new utility files

---
+++ src/lib/http.tsx
@@ -0,0 +1,52 @@
+import type { InternalAxiosRequestConfig } from 'axios'
+import axios, { isAxiosError } from 'axios'
+import { HTTPError } from '@/types/error'
+import { NProgressCustom } from './nprogress'
+
+const nprogress = NProgressCustom()
+
+const http = axios.create({
+  baseURL: '/api/v1',
+  timeout: 20000,
+  headers: { 'Content-Type': 'application/json' },
+  withCredentials: true,
+})
+
+http.interceptors.request.use(
+  (config: InternalAxiosRequestConfig) => {
+    if (config.headers['x-api'] === 'hardware') {
+      config.baseURL = import.meta.env.VITE_API_URL_HARDWARE
+    }
+    else {
+      config.baseURL = import.meta.env.VITE_API_URL_SOFTWARE
+    }
+    if (!config.doNotShowLoading) {
+      nprogress.start()
+    }
+    return config
+  },
+  (err) => {
+    return Promise.reject(err)
+  },
+)
+
+http.interceptors.response.use(
+  (response) => {
+    nprogress.done()
+    return Promise.resolve(response.data)
+  },
+  (error) => {
+    nprogress.done()
+
+    if (isAxiosError(error) && error.response && error.response.data) {
+      const { status } = error.response
+      const { message } = error.response.data
+
+      return Promise.reject(new HTTPError(message || 'Unexpected error', status))
+    }
+
+    return Promise.reject(error)
+  },
+)
+
+export default http
